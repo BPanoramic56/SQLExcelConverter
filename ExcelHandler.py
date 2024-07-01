e08dad47e4eb15d92bb54f7e0060533d412a800b
brunogomespascotto <panoramic56@proton.me>
2024-07-01 11:39:05 -0600

    Most implementation done, working on finishing touches

--- ExcelHandler.py
+++ ExcelHandler.py
@@ -1,13 +1,17 @@
-import configparser
 from openpyxl import Workbook
-import openpyxl
 from openpyxl.worksheet.table import Table, TableStyleInfo
+from openpyxl import load_workbook
 import SQLHandler
+import openpyxl
+import configparser
+
 
 config = configparser.ConfigParser()
 config.read("configurations.ini")
 
-table_name = config["Database Information"]["Table"]
+TableName = config["Database Information"]["Table"]
+
+Mode = config["Type Information"]["Mode"]
 
 def setup():
     workbook = Workbook()
@@ -36,20 +40,38 @@ def add_rows(server_connection, sheet):
 def create_table(server_connection, header_count):
     row_count = SQLHandler.get_row_count(server_connection)[0]['COUNT(*)']
 
-    table = Table(displayName=f"{table_name}", ref=f"A1:{openpyxl.utils.get_column_letter(header_count)}{row_count}")
+    table = Table(displayName=f"{TableName}", ref=f"A1:{openpyxl.utils.get_column_letter(header_count)}{row_count}")
 
-    style = TableStyleInfo(name=f"{table_name}", showFirstColumn=True,
+    style = TableStyleInfo(name=f"{TableName}", showFirstColumn=True,
                         showLastColumn=False, showRowStripes=True, showColumnStripes=True)
     table.tableStyleInfo = style
     return table
 
 def save_file(sheet, table, workbook):
     sheet.add_table(table)
-    workbook.save(f"/Users/brunogomespascotto/Downloads/{Table}.xlsx")
+    workbook.save(f"/Users/brunogomespascotto/Downloads/{TableName}.xlsx")
 
 if __name__ == "__main__":
-    workbook, sheet, server_connection = setup()
-    header_count = add_headers(server_connection, sheet)
-    add_rows(server_connection, sheet)
-    table = create_table(server_connection, header_count)
-    save_file(sheet, table, workbook)
+    if Mode == "SQL-Excel":
+        workbook, sheet, server_connection = setup()
+        header_count = add_headers(server_connection, sheet)
+        add_rows(server_connection, sheet)
+        table = create_table(server_connection, header_count)
+        save_file(sheet, table, workbook)
+
+    elif Mode == "Excel-SQL":
+        server_connection = SQLHandler.create_server_connection()
+        index = 1
+        while SQLHandler.ensure_table_existence(server_connection, TableName):
+            TableName = f"{TableName}{index}"
+            index += 1
+        Workbook = load_workbook(filename = config["Excel Information"]["Workbook"])
+        Sheet = Workbook.worksheets[0]
+
+        # print(Sheet.tables.values[0].ref)
+
+        SQLHandler.create_table(server_connection, TableName, list(next(Sheet.iter_rows(min_row=1, max_row=1, values_only=True))))
+
+        for row in Sheet.iter_rows(values_only=True):
+            # print(list(row))
+            SQLHandler.add_row(server_connection, TableName, list(row))
--- SQLHandler.py
+++ SQLHandler.py
@@ -40,3 +40,28 @@ def get_row_count(server_connection):
     with server_connection.cursor() as cursor:
         cursor.execute(f"SELECT COUNT(*) FROM {Table}")
         return cursor.fetchall()
+
+def ensure_table_existence(server_connection, TableName):
+    with server_connection.cursor() as cursor:
+        cursor.execute(f"SELECT COUNT(*) FROM  information_schema.tables WHERE table_schema = '{Database}' AND table_name = '{TableName}';")
+        return (cursor.fetchall()[0]['COUNT(*)']) > 0
+    
+def create_table(server_connection, TableName, TableColumns):
+    columns = []
+    for name in TableColumns:
+        columns.append(f"{name} VARCHAR(255)")
+    columns_formatted = ", ".join(columns)
+    print(f"CREATE TABLE {TableName} ({columns_formatted});")
+    with server_connection.cursor() as cursor:
+        query = f"CREATE TABLE {TableName} ({columns_formatted});"
+        cursor.execute(query)
+    server_connection.commit()
+
+def add_row(server_connection, table_name, value_list):
+    print(f"INSERT INTO {table_name} VALUES {value_list});")
+    columns_formatted = f", ".join([f'"{element}"' for element in value_list])
+    print(f"INSERT INTO {table_name} VALUES ({columns_formatted});")
+    with server_connection.cursor() as cursor:
+        query = f"INSERT INTO {table_name} VALUES ({columns_formatted});"
+        cursor.execute(query)
+    server_connection.commit()
